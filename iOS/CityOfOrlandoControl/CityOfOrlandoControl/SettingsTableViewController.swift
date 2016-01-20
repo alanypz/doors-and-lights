d0f31df87e0a664fa6013229003ca07afa0bb9ee
alanypz <alanypz@gmail.com>
2016-01-20 16:18:56 -0500

    First commit of Xcode Project. Log-in screen, Component view.

---
+++ iOS/CityOfOrlandoControl/CityOfOrlandoControl/SettingsTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsViewController.swift
+//  CityOfOrlandoControl
+//
+//  Created by Alan Yepez on 1/19/16.
+//  Copyright © 2016 cop4935-coo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelSettingsPopup(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func doneSettingsPopup(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
